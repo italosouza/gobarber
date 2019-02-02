6f5808f682c68a1ca1a87ee149a736ba0b534fbe
Italo Souza <italoasouza@gmail.com>
2019-02-02 17:15:24 -0200

    upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const routes = express.Router()
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
-routes.get('/', (req, res) => {
-  res.render('auth/singup')
-})
+const routes = express.Router()
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', upload.single('avatar'), UserController.store)
 
 module.exports = routes
