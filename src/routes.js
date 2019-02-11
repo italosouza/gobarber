dbb1fab58306922ffcd1f045d71fd84e79a5a7ad
Italo Souza <italoasouza@gmail.com>
2019-02-10 23:28:04 -0200

    login

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 const UserController = require('./app/controllers/UserController')
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', SessionController.create)
 
 routes.get('/signup', UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
