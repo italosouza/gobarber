f26bff1850cb58970f848429c804412a8e5c22c8
Italo Souza <italoasouza@gmail.com>
2019-02-15 00:04:25 -0200

    dashboard, autenticacao

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,35 @@ const multerConfig = require('./config/multer')
 const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
+
+const authMiddleware = require('./app/middleware/auth')
+const guestMiddleware = require('./app/middleware/guest')
+
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
+const DashboardController = require('./app/controllers/DashboardController')
+const FileController = require('./app/controllers/FileController')
 
-routes.get('/', SessionController.create)
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
 
-routes.get('/signup', UserController.create)
+  return next()
+})
+
+// rota para os arquivos da pasta upload
+routes.get('/files/:file', FileController.show)
+
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+// garante que todas as rotas que iniciem com /app utilizem o middleware de auth
+routes.use('/app', authMiddleware)
+
+routes.get('/app/logout', SessionController.destroy)
+routes.get('/app/dashboard', DashboardController.index)
 
 module.exports = routes
